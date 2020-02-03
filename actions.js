06c8615cc78d799dc24846d2cdd531b3e1c945af
Alexa Andrews <aandrews4@zagmail.gonzaga.edu>
2020-02-02 21:56:13 -0800

    save tour initial, needs location

--- actions.js
+++ actions.js
@@ -8,6 +8,8 @@ var edit = false;
 // variables for the map
 var map, currentLocation, infoWindow, selectedLocation;
 
+// variables for the added media and added tours 
+var addedMedia, addedTours;
 
 // variables to hold the list of created media, stops, and tours
 // these will be filled with information from the database
@@ -75,6 +77,11 @@ window.addEventListener("load", function () {
         $(this).addClass('bg-info').siblings().removeClass('bg-info');
     });
 
+    // confirming add stop puts it in the table
+    $('#confirm-add-stop').click(function() {
+        var existingStopSelect = document.getElementById("existing-stops");
+        updateStopTable(existingStopSelect.value);
+    })
 
     // MARK: stop page event listeners
 
@@ -99,10 +106,47 @@ window.addEventListener("load", function () {
     // to the tour page
     $('#save-stop').click(function(e) {
         e.preventDefault();
-        $('#nav-pills a[href="#tour-page"]').tab('show');
-        edit = true;
-        initTourMap();
+
+        var title = document.getElementById("stop-title");
+        var description = document.getElementById("stop-description")
+        var titleValue = title.value;
+        var descriptionValue = description.value;
+
+        var mediaTable = document.getElementById("stop-media")
+        numRows = mediaTable.rows.length;
+        addedMedia = []
+        for (var i = 0; i < numRows; i++) {
+            addedMedia.push(mediaTable.rows[i].cells[0].innerHTML);
+        }
+        console.log("addedMedia")
+        // TODO: Get current selected location, or error otherwise
+
+        if (!titleValue) { // there must be a title
+            $("#stop-title").popover({ title: 'Error', content: "Title required"});
+            $("#stop-title").click();
+        } else if (Object.keys(existingMedia).includes(titleValue)) { // title must be unique
+            $("#stop-title").popover({ title: 'Error', content: "Title must be unique"});
+            $("#stop-title").click();
+        } else {
+            // save the media item
+            existingStops[titleValue] = {"description": descriptionValue}
+            // make an option in the add media modal's dropdown
+            var existingMediaSelect = document.getElementById("existing-stops");
+            var option = document.createElement('option');
+            option.text = option.value = titleValue;
+            option.selected = true; // the newly created stop should be selected
+            existingMediaSelect.add(option);
+            // navigate back to the tour page
+            $('#nav-pills a[href="#tour-page"]').tab('show');
+            initTourMap()
+            $('#add-stop-popup').modal('show'); // bring back up the modal
+        }
     });
+
+    // Remove the warning next time the media-title box is clicked
+    $('#stop-title').on('input', function(e) {
+        $("#stop-title").popover('dispose');
+    }); 
 
     // clicking a row in the media table highlights it
     $('#media-table').on('click', '.clickable-row', function(e) {
@@ -155,28 +199,29 @@ window.addEventListener("load", function () {
     });
 
     // Remove the warning next time the media-title box is clicked
-    $('#media-title').on('input', function(e) {
+    $('#media-title').on('input', function() {
         $("#media-title").popover('dispose');
     }); 
-
-
-    // TODO: when a file is uploaded it should be displayed
-    // $("#media-item").change(function() {
-    //     loadFile(this);
-    // });
-
-    // function loadFile(input) {
-    //     if (input.files && input.files[0]) {
-    //         var reader = new FileReader();
-            
-    //         reader.onload = function (e) {
-    //             $('#media-preview').attr('src', e.target.result);
-    //         }
-    //         reader.readAsDataURL(input.files[0]);
-    //     }
-    // };
 });
 
+
+function loadFile(e) {
+    var preview = document.getElementById('media-preview');
+    preview.src = URL.createObjectURL(e.target.files[0]);
+}
+
+function updateStopTable(name) {
+    var stopTable = document.getElementById("tour-stops");
+    var row = stopTable.insertRow(0);
+    row.className = 'clickable-row';
+    var cell = row.insertCell(0);
+    cell.innerHTML = name;
+    // TODO: Add event listeners when a stop is double clicked
+    // to display the tour's description and a list of 
+    // images and their descriptions in the popup
+    // add event listeners when a stop is single clicked to show its
+    // location on the map
+}
 
 function updateMediaTable(name) {
     var mediaTable = document.getElementById("stop-media");
